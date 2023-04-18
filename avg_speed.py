7a3766122ad223e8d562c3dfb4ac4a0c60de0d73
almohd1411 <129746629+almohd1411@users.noreply.github.com>
2023-04-18 16:54:21 +0300

    Create avg_speed.py

---
+++ avg_speed.py
@@ -0,0 +1,6 @@
+distance = float(input("Введите расстояние в км: "))
+time = float(input("Введите время в часах: "))
+
+avg_speed = distance / time
+
+print("Средняя скорость движения: {:.2f} км/ч".format(avg_speed))
